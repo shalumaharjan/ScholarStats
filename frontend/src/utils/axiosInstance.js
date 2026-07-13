06ba9fb5d06bd653b973074d724be5e5d4c7abcc
Shalu <shalumaharjan15@gmail.com>
2026-07-13 22:21:53 +0545

    feat: add axios instance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,2 +1,10 @@
-// api call
-// connect backend
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
