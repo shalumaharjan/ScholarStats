31adb8ea8cb1c4d6dcd01e4583946814d59384ea
Shalu <shalumaharjan15@gmail.com>
2026-07-15 18:54:06 +0545

    refactor: update axios API config and move app setup to main.jsx

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_FASTAPI_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
