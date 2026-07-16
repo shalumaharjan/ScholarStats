9a12a56b75cbcda984f79531d955cf0a6ae5797f
anjilabhatt <bhattanjila98@gmail.com>
2026-07-16 23:11:32 +0545

    File structure modify

--- backend/API/database.py
+++ backend/API/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./app.db"  # creates app.db file
+DATABASE_URL = "sqlite:///./api/utils/mydb.db"  # creates .db file
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
